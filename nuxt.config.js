2a2b9490ea5e573f82634698ede96b57c194ffe2
Giedrius R <gr@veikia.net>
2021-05-20 20:03:50 +0300

    ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  //ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
